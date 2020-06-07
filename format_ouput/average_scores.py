002c744525be3670f43529dc9ece182935813e28
CS-41 <eallen12@dmacc.edu>
2020-06-07 18:39:26 -0500

    Commit

--- format_ouput/average_scores.py
+++ format_ouput/average_scores.py
@@ -3,9 +3,9 @@ Program: average_scores.py
 Author: Elizabeth Allen
 Last date modified: 06/07/2020
 
-The purpose
-The input
-The output
+The purpose of this program is to show the average of three test scores.
+The inputs are first name, last name, age, three test scores.
+The output shows the first name, last name, age and the average of the test scores.
 """
 
 import unittest
@@ -13,23 +13,32 @@ import unittest
 if __name__ == '__main__':
     first_name = input("Enter your first name: ")  # Input first name
     last_name = input("Enter your last name: ")  # Input last name
-    user_age = input("Enter your age: ")  # Input age
-    score1 = int(input("Enter 1st score: "))  # Input test scores
-    score2 = int(input("Enter 2nd score: "))
-    score3 = int(input("Enter 3rd score: "))
+    user_age = int(input("Enter your age: "))  # Input age
+    score1 = float(input("Enter 1st score: "))  # Input test scores. Float used for partial credit scores.
+    score2 = float(input("Enter 2nd score: "))
+    score3 = float(input("Enter 3rd score: "))
     score_list = [score1, score2, score3]
 
-    a = int(sum(score_list))
-    b = int(len(score_list))
-    c = a // b
+    a = sum(score_list)  # Sums list
+    b = len(score_list)  # Counts values in list
+    test_average = a / b
 
 
-    def average(c):
-        return c
+    def average(test_average):
+        return test_average
 
 
-    average_scores = average(c)
+    average_scores = average(test_average)
 
-    print(f"Name: {last_name},{first_name}; age: {user_age}; average score: {average_scores}")
+    # Output formats the average to print only 2 decimal places
+    print("Name: {0},{1}; age: {2}; average score: {3:.2f}".format(last_name, first_name, user_age, test_average))
 
 unittest.main()
+
+# Tests (focused on averages)
+# score1    score2   score3     expected output     actual output
+#   50        50       50             50.00             50
+#  24.5     60.25     75.75           53.50             53.50
+#  100        97        72            89.67             89.67
+#   9         10        8.5           9.17              9.17
+#   75      84.50     94.25           84.58             84.58
