7fbfb6234df0adde9e856337d5d14d5a4f45cc81
CS-41 <eallen12@dmacc.edu>
2020-06-07 16:19:38 -0500

    Commit

--- test/test_average_scores.py
+++ test/test_average_scores.py
@@ -1,3 +1,13 @@
+"""
+Program: test_average_scores.py
+Author: Elizabeth Allen
+Last date modified: 06/07/2020
+
+The purpose of this program is to show the average test file.
+The input is side_effect=[85,90,95].
+The output will be an error, per the instructions.
+"""
+
 import unittest.mock as mock
 
 from format_output.average_scores import average as avg
